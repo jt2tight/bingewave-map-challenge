16d43bc24aae726b56c07a60918e1eccfba54f62
James <88163054+jt2tight@users.noreply.github.com>
2021-09-18 21:28:42 -0400

    Added Widget
    Updated Map
    Added Widget
    Added Chatbot

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,27 @@ WA.room.onEnterZone('clock', () => {
     currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
 })
 
+WA.room.onEnterZone('bingewaveZone', ()=> {
+    setTimeout(() => WA.chat.sendChatMessage('If the video does not auto-play you will need to start the stream here: https://www.livestream-demo.com/content-creators/', 'Bingewave Bot'), 3000)
+})
+
+WA.room.onEnterZone('coffeeRoom', ()=> {
+    currentPopup = WA.ui.openPopup("coffeePopup","Outta cofee...", [])
+})
+
+WA.room.onEnterZone('workdesks', ()=> {
+    currentPopup = WA.ui.openPopup("workPopup","I don't want to work right now", [])
+})
+
 WA.room.onLeaveZone('clock', closePopUp)
+
+WA.room.onLeaveZone('coffeeRoom', closePopUp)
+
+WA.room.onLeaveZone('workdesks', closePopUp)
+
+WA.room.onLeaveZone('bingewaveZone', closePopUp)
+
+WA.chat.sendChatMessage('Welcome to the online conference! Have a look around!', 'Bingewave Bot');
 
 function closePopUp(){
     if (currentPopup !== undefined) {
